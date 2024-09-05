ff88f1e1724ca04687f753fb446694270d69ff75
Pedro <phpontesmartins19@gmail.com>
2024-09-05 10:59:19 -0300

    :sparkles: feat: Adição da StatusBar

--- src/app/home/styles.ts
+++ src/app/home/styles.ts
@@ -1,7 +1,14 @@
 import { StyleSheet } from "react-native";
 
+import { theme } from "@/themes";
+
 export const styles = StyleSheet.create({
     container: {
         flex: 1,
-    }
+    },
+    header: {
+        width: "100%",
+        height: 132,
+        backgroundColor: theme.colors.blue,
+    },
 })
